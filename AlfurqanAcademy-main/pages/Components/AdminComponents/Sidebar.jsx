3e5443171fc8fb7e8183c1f618b0b2995a6cbb4a
Ansalin789 <ansalinsagar789@gmail.com>
2024-11-30 16:05:49 +0530

    kdndjdkz

--- AlfurqanAcademy-main/pages/Components/AdminComponents/Sidebar.jsx
+++ AlfurqanAcademy-main/pages/Components/AdminComponents/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './Sidebar.module.css'; // Adjust the path if needed
-import { assets } from '@/Assets/assets';
+// import { assets } from '@/Assets/assets';
 
 const Sidebar = () => {
     return (
